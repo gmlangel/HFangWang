0584e461113ab069de541f57f8a76c7072e4bafc
guominglong <guominglong@guominglongdemacbook-pro.local>
2016-02-01 18:17:45 +0800

    Initial Commit

---
+++ HFangWang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HFangWang
+//
+//  Created by guominglong on 16/2/1.
+//  Copyright © 2016年 guominglong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
